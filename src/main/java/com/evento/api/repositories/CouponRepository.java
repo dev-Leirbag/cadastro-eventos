f1984a3afb629c9e0d5e4d411495513fbe806269
dev-LierBag <gabrielaf2186@gmail.com>
2025-07-17 02:26:36 -0300

    feat: add repositories

---
+++ src/main/java/com/evento/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.evento.api.repositories;
+
+import com.evento.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
